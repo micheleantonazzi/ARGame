9873bedb2b471c1fdc371744e06c3817cd956d27
michele <micheleantonazzi@gmail.com>
2020-10-06 12:15:26 +0200

    MainActivity: added login intent

--- code/android-native/app/src/main/java/com/argame/MainActivity.java
+++ code/android-native/app/src/main/java/com/argame/MainActivity.java
@@ -2,13 +2,72 @@ package com.argame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth authService;
+    private static final int SIGN_IN_CODE = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        this.authService = FirebaseAuth.getInstance();
+    }
+
+    // Check if the user is logged, otherwise load the login intent
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = this.authService.getCurrentUser();
+        if (currentUser == null)
+            createSignInIntent();
+    }
+
+    // Create the login intend provided by Firebase
+    public void createSignInIntent(){
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build(),
+                new AuthUI.IdpConfig.EmailBuilder().build());
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setTosAndPrivacyPolicyUrls(
+                                "https://example.com/terms.html",
+                                "https://example.com/privacy.html")
+                        .build(),
+                SIGN_IN_CODE);
+    }
+
+    // Obtain the result of the Firebase's login intent
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == SIGN_IN_CODE) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+            } else {
+
+            }
+        }
     }
 }
