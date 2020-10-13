900105d12d0498a82c1bdfdaabe1c22c90a59365
michele <micheleantonazzi@gmail.com>
2020-10-13 18:00:57 +0200

    AccountSettingsActivity.java: added profile photo

--- code/android-native/app/src/main/java/com/argame/activities/settings/AccountSettingsActivity.java
+++ code/android-native/app/src/main/java/com/argame/activities/settings/AccountSettingsActivity.java
@@ -2,12 +2,16 @@ package com.argame.activities.settings;
 
 import android.os.Bundle;
 
+import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.argame.R;
 
@@ -20,9 +24,9 @@ public class AccountSettingsActivity extends AppCompatActivity {
 
         // Add back button
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        CollapsingToolbarLayout toolBarLayout = findViewById(R.id.collapsing_layout_app_bar_account_activity);
         toolBarLayout.setTitle(getTitle());
 
         // Setup back button
@@ -31,6 +35,16 @@ public class AccountSettingsActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
+
+        // Modify alpha while scrolling
+        LinearLayout linearLayoutStatusBar = findViewById(R.id.linear_layout_status_bar_account_activity);
+        AppBarLayout appBarLayout = findViewById(R.id.app_bar_layout_account_activity);
+        ImageView imageViewProfilePhoto = findViewById(R.id.image_view_profile_photo);
+        appBarLayout.addOnOffsetChangedListener((layout, verticalOffset) -> {
+            float offsetAlpha = (layout.getY() / layout.getTotalScrollRange());
+            linearLayoutStatusBar.setAlpha(1);
+            imageViewProfilePhoto.setAlpha(1.0f + offsetAlpha * 1.5f);
+        });
 
     }
 }
