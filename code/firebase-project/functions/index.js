502b90647e560b6e34ed9cadd4c2283ea46c5764
michele <micheleantonazzi@gmail.com>
2020-10-15 11:00:33 +0200

    firebase-project: created cloud function to add data about new users

--- code/firebase-project/functions/index.js
+++ code/firebase-project/functions/index.js
@@ -1,9 +1,20 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+exports.onCreateNewUser = functions.auth.user().onCreate(async (user) => {
+    const database = admin.firestore();
+
+    // Add new user to the database
+    const userRef = database.collection('users').doc(user.uid);
+
+    await userRef.set({
+        name: user.displayName === null ? "" : user.displayName.split(' ')[0],
+        surname: user.displayName === null ? "" : user.displayName.split(' ')[1],
+        email: user.email,
+        nickname: "",
+        profile_image: user.photoURL === null ? -1 : 0
+    });
+});
