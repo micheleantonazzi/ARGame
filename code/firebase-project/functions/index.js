a92635655012173c280fc2e6ef3465271bd35c81
michele <micheleantonazzi@gmail.com>
2020-10-15 17:31:57 +0200

    cloud_functions: now the profile image is uploaded (if I had a billing account) to storage and the user data to cloud firestore

--- code/firebase-project/functions/index.js
+++ code/firebase-project/functions/index.js
@@ -2,19 +2,41 @@ const functions = require('firebase-functions');
 
 const admin = require('firebase-admin');
 
-admin.initializeApp();
+const serviceAccount = require('./arvideocall-38d60-firebase-adminsdk-jokf6-dbb11cccc4.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: 'gs://arvideocall-38d60.appspot.com'
+});
 
 exports.onCreateNewUser = functions.auth.user().onCreate(async (user) => {
-    const database = admin.firestore();
+
+    // Copy profile image to storage if exists
+    if(user.photoURL !== null){
+        const axios = require('axios');
+        axios.get(user.photoURL, {responseType: 'arraybuffer'})
+            .then(response => {
+                const imageFile = admin.storage().bucket().file('users/' + user.uid + '/images/profile_photo.png');
+                const options = {
+                    metadata: {contentType: 'image/png'}
+                };
+
+                imageFile.save(response.data, options)
+                    .then(() => console.log('Done'))
+                    .catch((e) => console.log('Upload fail ' + e.toString()));
+                return null;
+            })
+            .catch(e => console.log('Upload image profile failed: ' + e.toString()));
+    }
 
     // Add new user to the database
-    const userRef = database.collection('users').doc(user.uid);
-
-    await userRef.set({
+    const userRef = admin.firestore().collection('users').doc(user.uid);
+    return userRef.set({
         name: user.displayName === null ? "" : user.displayName.split(' ')[0],
         surname: user.displayName === null ? "" : user.displayName.split(' ')[1],
         email: user.email,
         nickname: "",
         profile_image: user.photoURL === null ? -1 : 0
-    });
+    }).then(() => console.log('New user created'))
+        .catch(e => 'Creation failed: ' + e);
 });
